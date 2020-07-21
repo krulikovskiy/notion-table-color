880d43b3438d8a629a0ebe3e0ba827c68486a62d
krulikovskiy <it@krulikovskiy.com>
2020-07-21 17:16:53 +0300

    выгрузка винда

--- contentScript.js
+++ contentScript.js
@@ -21,12 +21,12 @@ setTimeout(function () {
     NCJ('.notion-table-view > .notion-selectable:not(.notion-collection-item)').each(function () {
             if (color_start.indexOf(NCJ(this).data('block-id')) != -1) {
                 NCJ(this).addClass('notion-color-table');
-                let arrs = NCJ(this).find("div:contains('{color}')");
-                if (arrs.length >= 6) {
-                    arrs.splice(0, 2);
-                    for (let i = 0; i < arrs.length / 6; i++) {
+                let arrs_sort = NCJ(this).find("div:contains('{sort-color}')");
+                if (arrs_sort.length >= 6) {
+                    arrs_sort.splice(0, 2);
+                    for (let i = 0; i < arrs_sort.length / 6; i++) {
                         let vector = 'min';
-                        let NCJhead = NCJ(arrs[i * 5 + 2]);
+                        let NCJhead = NCJ(arrs_sort[i * 5 + 2]);
                         NCJhead.css({color: "red"});
                         if (NCJhead.text().indexOf('{color-min-max}') != -1) {
                             vector = 'color-min-max'
@@ -34,7 +34,7 @@ setTimeout(function () {
                         if (NCJhead.text().indexOf('{color-max-min}') != -1) {
                             vector = 'color-max-min'
                         }
-                        let index = NCJ(arrs[i * 5]).index();
+                        let index = NCJ(arrs_sort[i * 5]).index();
                         let values = NCJ(this).find('.notion-collection-item > div:nth-child(' + (index + 1) + ')');
                         if (values.length > 0) {
                             let min = 0;
@@ -71,6 +71,43 @@ setTimeout(function () {
                                 }
                                 NCJ(this).css({background: getColor((value - min) / mid, vector)});
 
+                            })
+                        }
+                    }
+                }
+                let arrs_color = NCJ(this).find("div:contains('{bool-color:')");
+                if (arrs_color.length >= 6) {
+                    arrs_color.splice(0, 2);
+                    let name = NCJ(arrs_color[0]).text();
+                    let req = Array.from(name.matchAll(/.*\{bool-color:(.*):(.*)\}/g));
+                    if (req.length == 1) {
+                        let color_off = req[0][1];
+                        let color_on = req[0][2];
+                        for (let i = 0; i < arrs_color.length / 6; i++) {
+                            let index = NCJ(arrs_color[i * 5]).index();
+                            let values = NCJ(this).find('.notion-collection-item > div:nth-child(' + (index + 1) + ')');
+                            values.each(function () {
+                                if (NCJ(this).find('.check').length > 0) {
+                                    NCJ(this).css({background: color_off});
+                                } else {
+                                    NCJ(this).css({background: color_on});
+                                }
+                            })
+                        }
+                    }
+                }
+                let arrs_bool = NCJ(this).find("div:contains('{color:')");
+                if (arrs_color.length >= 6) {
+                    arrs_color.splice(0, 2);
+                    let name = NCJ(arrs_color[0]).text();
+                    let req = Array.from(name.matchAll(/.*\{color:(.*)\}/g));
+                    if (req.length == 1) {
+                        let color = req[0][1];
+                        for (let i = 0; i < arrs_color.length / 6; i++) {
+                            let index = NCJ(arrs_color[i * 5]).index();
+                            let values = NCJ(this).find('.notion-collection-item > div:nth-child(' + (index + 1) + ')');
+                            values.each(function () {
+                                NCJ(this).css({background: color});
                             })
                         }
                     }
